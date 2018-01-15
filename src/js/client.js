1ee93563e8a5230d6f5df1d8a3fa089d61bc873d
jerilkuriakose <jeril.work@gmail.com>
2018-01-15 16:25:37 +0530

    initial commit

--- src/js/client.js
+++ src/js/client.js
@@ -1,1 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+class SampleComp extends React.Component {
+  render() {
+    return (
+        <h1>Sample text</h1>
+    )
+  }
+}
+const app = document.getElementById('app');
+ReactDOM.render(<SampleComp />, app);
 
